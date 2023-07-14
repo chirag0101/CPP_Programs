83a8adbc3fd692b18d6ed0b2207b29f423520ccc
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-07-14 21:42:43 +0530

    const

--- constructor.cpp
+++ constructor.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 
-int main(){
+class Demo{
+    int x=10;
+    int y=20;
 
+public:
+    void fun(){
+        std::cout<<x<<std::endl;
+        std::cout<<y<<std::endl;
+
+    }
+}; 
+
+int main(){
+    Demo obj;
     return 0;
 }
---
+++ staticVariables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+class Demo{
+    int x=10;
+//static int y=20;                      //initialization of static variable is not allowed in c++
+    static int y;
+
+public:
+    void fun(){
+        std::cout<<x<<std::endl;
+        std::cout<<y<<std::endl;    //undefined reference to undefined Demo::y
+    }
+}; 
+
+int main(){
+    Demo obj;
+    return 0;
+}
