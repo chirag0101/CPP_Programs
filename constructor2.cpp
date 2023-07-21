527157cda92902a508ca6509d1c95127647bfa5d
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-07-21 23:06:45 +0530

    constructor2

--- constructor2.cpp
+++ constructor2.cpp
@@ -13,13 +13,17 @@ class Demo{
             std::cout<<x<<std::endl;
         }
 
+        Demo(Demo &xyz){
+            std::cout<<"Copy Constructor!"<<std::endl;
+        }
+
 };
 
 int main(){
     
     Demo obj1;                                              //calls No-args constructor
     
-    Demo obj2();                                            //warning! 
+    Demo obj2();                                            //warning! & no output cz it's the declaration of a function
     
     Demo obj3{10};                                        //calls No-args constructor
 
@@ -28,6 +32,10 @@ int main(){
     Demo *obj5=new Demo();                      //calls No-args constructor
 
     Demo *obj6=new Demo(10);                  //calls parameterized constructor
+
+    Demo obj7(obj4);                                    //copy constructor for object on stack frame
+
+    Demo obj8=obj4;                                    //copy constructor, same as obj7
 
     return 0;
 }
