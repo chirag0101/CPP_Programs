77a0b6af53c21d605fd733b2e7e2ba9ad1b17c4d
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-07-22 21:27:01 +0530

    copy constructor

---
+++ constructor3.cpp
@@ -0,0 +1,41 @@
+//copy constructor main use
+
+#include<iostream>
+
+class Demo{
+    public:
+        int x=10;
+
+        Demo(){
+            std::cout<<"No-args Constructor"<<std::endl;
+        }
+
+        Demo(int x){
+            std::cout<<"Para Constructor"<<std::endl;
+        }
+
+        Demo(Demo &xyz){
+            std::cout<<"Copy Constructor"<<std::endl;
+        }
+
+        void fun(){
+            std::cout<<x<<std::endl;
+            std::cout<<this->x<<std::endl;
+        }
+        
+};
+
+int main(){
+
+    Demo obj1;
+    
+    Demo obj2=obj1;
+    
+    obj1.x=20;
+    obj2.x=30;
+    
+    obj1.fun();
+    obj2.fun();
+    
+    return 0;
+}
