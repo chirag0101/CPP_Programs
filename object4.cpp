8991cb9ae2042603340066178fb845afc9851db2
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-07-23 18:20:23 +0530

    obj4

--- object4.cpp
+++ object4.cpp
@@ -2,19 +2,35 @@
 
 #include<iostream>
 
+using namespace std;
+
 class Demo{
+    
+    int x=10;
+    int y=20;
     
     public:
         
         Demo(){
+            cout<<"NO-Args"<<endl;
+        }
 
+        Demo(int x=50,int y=30){
+            this->x=x;
+            this->y=y;
+            cout<<"Para"<<endl;
+            cout<<x<<y<<endl;
         }
 
 };
 
 int main(){
     
-    Demo obj1;
+    //Demo obj1;          //ambiguity as can't decide which constructor to call
+
+    Demo obj2(90,100);
+    
+    Demo obj3(200);
 
     return 0;
 }
