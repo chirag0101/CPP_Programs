2fe2b41d1160952c943a7dca4fa602575fc76e1b
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-07-24 15:29:40 +0530

    constObj

--- destructor2.cpp
+++ destructor2.cpp
@@ -1,12 +1,28 @@
 #include<iostream>
+
 using namespace std;
+
 class Demo{
+
     public:
-        
+
+        Demo(){
+            cout<<"Constructor"<<endl;
+        }
+    
+        ~Demo(){
+            cout<<"Destructor"<<endl;
+        }
+
 };
 
 int main(){
+
 	Demo obj1;
-	Demo obj2=obj1;
-	return 0;
+    
+    Demo *obj2=new Demo();
+    
+    delete(obj2); 
+	
+    return 0;
 }
