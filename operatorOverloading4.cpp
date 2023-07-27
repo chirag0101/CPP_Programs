5398074249dd54ea18f24ce9fb54fa289d3a2ac0
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-07-27 20:29:58 +0530

    oprOverloading

--- operatorOverloading4.cpp
+++ operatorOverloading4.cpp
@@ -1,11 +1,29 @@
-#include <iostream>
+//Overloading arithmetic operators.
+
+#include<iostream>
+
+class Demo{
+    
+    int x=10;
+    
+    public:
+    
+        Demo(int x){
+            this->x=x;
+        }
+
+        friend int operator+(const Demo &obj1,const Demo &obj2){
+            return obj1.x+obj2.x;                                                             //it calls the predefined function having parameter (int,int)
+        }
+
+};
 
 int main(){
 
-    int x=10;
-    
-    std::cout<<x<<std::endl;
-    
-    return 0;
+    Demo obj1(30);
+    Demo obj2(40);
 
+    std::cout<<obj1+obj2<<"\n";
+
+    return 0;
 }
