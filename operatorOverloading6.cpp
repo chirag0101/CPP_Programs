be7b080fcd241feec1a1f6ad45bc56324fd11bd4
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-07-27 22:29:52 +0530

    oprOverloading

--- operatorOverloading6.cpp
+++ operatorOverloading6.cpp
@@ -11,11 +11,23 @@ class Demo{
             this->x=x;
         }
 
+        int getData() const{
+            return x;
+        }
+
 };
 
-
+int operator+(const Demo &obj1,const Demo &obj2){
+    return obj1.getData() + obj2.getData();
+}
 
 int main(){
+
+    Demo obj1(30);
+
+    Demo obj2(40);
+
+    std::cout<<obj1+obj2<<std::endl;
 
     return 0;
 
