49d88f9dce839e6ee99e25ba626cd486e7160229
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-07-28 15:08:52 +0530

    oprOverload

--- operatorOverloading11.cpp
+++ operatorOverloading11.cpp
@@ -1,12 +1,33 @@
 //overloading relational operators <,>,<=,>=,==,!=
 
+//overlading '<' here
+
 #include<iostream>
 
 class Demo{
 
+    int x=10;
+
+    public:
+        Demo(int x){
+            this->x=x;
+        }
+
+    friend int operator<(const Demo &obj1,const Demo &obj2){
+        if(obj1.x<obj2.x){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
 };
 
 int main(){
+    Demo obj1(30);
+    Demo obj2(20);
+
+    std::cout<<(obj1<obj2)<<std::endl;
 
     return 0;
 }
