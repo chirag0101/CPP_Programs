0ad0e984d52f9c36e4d7b154dcf6f20b27c32162
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-07-28 16:43:33 +0530

    oprOverload

--- operatorOverloading13.cpp
+++ operatorOverloading13.cpp
@@ -1,1 +1,42 @@
 //Overloading new & delete operators
+
+#include<iostream>
+#include<stdlib.h>
+
+class Demo{
+    int x;
+    
+    public:
+        
+        Demo(int x){
+            this->x=x;
+        }
+
+        void getData(){
+            std::cout<<x<<std::endl;
+    }
+
+        ~Demo(){
+            std::cout<<"des"<<std::endl;
+        }
+
+        friend void* operator new(size_t size){
+            void *ptr=malloc(size);
+            //internally: void *ptr=::operator new(size)                :: goes to global namespace
+            return ptr;
+        }
+};
+
+int main(){
+ 
+    Demo *obj=new Demo(50);
+
+    //this goes as->    new(sizeof(Demo))
+    //then                  obj(50)
+
+    obj->getData();
+ 
+    delete(obj);
+ 
+    return 0;
+}
