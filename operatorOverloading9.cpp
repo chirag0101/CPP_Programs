4a4326384bfafc3de62c9ef5f9fda66238f29459
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-07-28 11:38:03 +0530

    operatorOverloading

--- operatorOverloading9.cpp
+++ operatorOverloading9.cpp
@@ -2,11 +2,19 @@
 #include<iostream>
 
 class Demo{
+    int x=10;
 
+    friend std::ostream& operator<<(std::ostream &xyz,const Demo &obj){
+        xyz<<obj.x;                 //again goes as: ostream& operator<<(ostream &xyz,int obj.x);
+        return xyz;
+    }
 };
 
 int  main(){
 
-    return 0;
+    Demo obj1;
 
+    std::cout<<obj1<<std::endl;
+
+    return 0;
 }
