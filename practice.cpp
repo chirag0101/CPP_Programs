1de94585e9f2e7d06492717f2566873f90c4f120
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-07-30 22:22:02 +0530

    prac

--- practice.cpp
+++ practice.cpp
@@ -1,29 +1,11 @@
 #include<iostream>
-using namespace std;
-class Demo{
-    public:
-
-    Demo(){
-        cout<<"Constructor."<<endl;
-    }
-
-    ~Demo(){
-        cout<<"Destructor."<<endl;        
-    }
-
-    friend void* operator new(size_t size){
-        void *ptr=malloc(size);
-        cout<<"new"<<endl;
-        return ptr;
-    }
-
-    friend void operator delete(void* ptr){
-        cout<<"delete"<<endl;
-        free(ptr);
-    }
-};
-
-int main(){
-    Demo *obj=new Demo();
-    delete(obj);
+void foo(int *x){
+    delete(x);
 }
+int main(){
+    int *ptr=new int;
+    *ptr=5;
+    foo(ptr);
+    std::cout<<*ptr<<std::endl;
+}
+// can you explain the output of the above code?
