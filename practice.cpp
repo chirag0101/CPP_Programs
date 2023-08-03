e1cbe008f7455fb54890ff1fab66a432635f1072
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-08-03 10:22:08 +0530

    03

--- practice.cpp
+++ practice.cpp
@@ -3,22 +3,36 @@
 class Demo{
     int x=10;
     int y=20;
-    // friend std::ostream& operator<<(std::ostream& cout,const Demo& obj){
-    //     std::cout<<obj.x<<" "<<obj.y;
-    //     return cout;
-    // }
+    
+    public:
+    
+    Demo(int x){
+        this->x=x;
+    }
+
+    friend int operator+(const Demo& obj1,const Demo& obj){
+        return obj.x+obj1.x;
+    }
+    
     public:
     void getData() const{
         std::cout<<x<<y;
     }
 };
+
 std::ostream& operator<<(std::ostream& cout,const Demo& obj){
     obj.getData();
     return cout;
 }
 
+
 int main(){
-    Demo obj;
-    std::cout<<obj<<std::endl;
+
+    Demo obj(10);
+    Demo obj1(20);
+    
+    std::cout<<obj+obj1<<std::endl;
+
     return 0;
+
 }
