a1669692438840878488a7c905a1b2e6f97722aa
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-08-10 00:03:03 +0530

    inherit

--- inheritance1.cpp
+++ inheritance1.cpp
@@ -3,11 +3,57 @@
 #include<iostream>
 
 class Emp{
+    std::string empName="chirag";
+    int empId=20;
 
+    public:
+        Emp(){
+            std::cout<<"Emp Constructor\n";
+        }
+
+        void getInfo(){
+            std::cout<<empName<<" "<<empId<<std::endl;
+        }
+
+        ~Emp(){
+            std::cout<<"Emp Destructor\n";
+        }
+
+};
+
+class Company{
+    std::string compName="jio";
+    int compEmp=500;
+
+    Emp obj;
     
+    public:
+
+    Company(std::string compName,int compEmp=0){
+        std::cout<<"Comp Constructor\n";
+        this->compName=compName;
+        this->compEmp=compEmp;
+    }
+
+    void getInfo(){
+        std::cout<<compName<<" "<<compEmp<<std::endl;
+        obj.getInfo();
+    }
+
+    ~Company(){
+            std::cout<<"Comp Destructor\n";
+    }
 };
 
 int main(){
+    Company obj1("rel",200);
 
     return 0;
 }
+
+/*op: comp const
+rel 200
+chirag 20
+emp des
+comp des
+*/
