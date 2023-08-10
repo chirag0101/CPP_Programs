752cf8f0c9c382bd941ac1a6d2cfd277858fb489
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-08-10 21:54:45 +0530

    inherit

--- inheritance5.cpp
+++ inheritance5.cpp
@@ -20,7 +20,7 @@ class Child : public Parent{
         Child(){
             std::cout<<"Child constructor called"<<std::endl;
         }
-        
+
         ~Child(){
             std::cout<<"Child destructor called"<<std::endl;
         }
@@ -28,9 +28,11 @@ class Child : public Parent{
 
 int main(){
 
-    Child c;
-    return 0;
-
+    Child c;                                    //1st parent constructor gets called then child as parent is initialized as instance data member 
+    Child *c2=new Child();
+    delete(c2);
+    
+    return 0;                                  //at last parent destructor gets called as we need to free the child class 1st as it can access parent class data which ain't even present 
 }
 
 /*op: parent const
