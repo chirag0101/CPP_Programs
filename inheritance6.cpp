daeb9b32aeed40e62fafcfdb6e016e5eb680e352
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-08-10 22:08:34 +0530

    inherit

---
+++ inheritance6.cpp
@@ -0,0 +1,40 @@
+//actual start to inheritance
+
+#include<iostream>
+
+class Parent{
+    
+    public:
+        Parent(){
+            std::cout<<"Parent constructor called"<<std::endl;
+        }
+    
+        ~Parent(){
+            std::cout<<"Parent destructor called"<<std::endl;
+        }
+};
+
+class Child : public Parent{
+    
+    public:
+        Child(){
+            std::cout<<"Child constructor called"<<std::endl;
+        }
+
+        ~Child(){
+            std::cout<<"Child destructor called"<<std::endl;
+        }
+};
+
+int main(){
+
+    Child c; 
+    Child *c2=new Child();
+    delete(c2);
+    
+    return 0;                                  
+}
+
+/*op: parent const
+        child const
+*/
