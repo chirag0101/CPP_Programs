246adddd379eabd457d4592daf9b151855d372b6
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-08-11 20:02:25 +0530

    inherit

--- inheritance11.cpp
+++ inheritance11.cpp
@@ -1,56 +1,56 @@
-//public inheritance
-
 #include<iostream>
 
 class Parent{
     int x=10;
-
-    protected:
-        int y=20;
     
     public:
-        int z=30;
 
         Parent(){
             std::cout<<"P Constructor"<<std::endl;
         }
-};
 
-class Child1 : public Parent{                    //public inheritance
-    public:
-        Child1(){
-            std::cout<<"C Constructor"<<std::endl;
+         virtual void getData(){
+            std::cout<<"Parent"<<x<<std::endl;
         }
 };
 
-class Child2 : private Parent{                    //private inheritance -> now all the data members are private within the child class
+class Child1 : public Parent{                    //public inheritance
+    int x=20;
+
+    public:
+        Child1(){
+            std::cout<<"C1 Constructor"<<std::endl;
+        }
+
+        void getData(){
+            std::cout<<"C1"<<x<<std::endl;
+        }
+
+};
+
+class Child2 : public Parent{                    //public inheritance
+    int x=30;
+    
     public:
         Child2(){
             std::cout<<"C2 Constructor"<<std::endl;
         }
-};
 
-
-class Child3 : protected Parent{                    //private inheritance -> now all the data members are private within the child class
-    public:
-        Child3(){
-            std::cout<<"C3 Constructor"<<std::endl;
+        void getData(){
+            std::cout<<"C2"<<x<<std::endl;
         }
+
 };
 
 int main(){
 
-    Child1 c1;
-    
-    std::cout<<c1.x<<c1.y<<c1.z;               //can't access private & protected data members of a class outside child class & z can be accessed
-    
-    Child2 c2;
-    
-    std::cout<<c2.x<<c2.y<<c2.z;               //can't access x,y,z as all become private in child class
-    
-    Child3 c3;
-    
-    std::cout<<c3.x<<c3.y<<c3.z;               //can't access x,y,z as all become protected in child class
-    
+    Parent *p1=new Child1();
+
+    p1->getData();                                       //this calls getData of Parent as it's referenced gets passed 
+
+    Parent *p2=new Child2();
+
+    p2->getData();                                       //this calls getData of C2 class as now the getData function is virtual
+
     return 0;
 }
