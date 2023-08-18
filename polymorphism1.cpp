68f0b15c4c3c4427d486f64da29502aa0ae14750
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-08-18 10:34:52 +0530

    polymorphism

--- polymorphism1.cpp
+++ polymorphism1.cpp
@@ -7,7 +7,7 @@ class Parent{
             std::cout << "Parent constructor called" << '\n';
         }
 
-        virtual void getData(){
+        virtual void getData(){                        //by using virtual keyword compiler gets to know that if there is any derived class of this class which has method of same namee then ignore this method nd go to the derived one
             std::cout<<"Parent GetData\n";
         }
 
