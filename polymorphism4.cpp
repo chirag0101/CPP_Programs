7d81e66f79ab42ff833094135f5eb894114ab41d
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-08-18 17:15:44 +0530

    polymorphism

--- polymorphism4.cpp
+++ polymorphism4.cpp
@@ -1,8 +1,26 @@
-//primitive return types & co-variant return types
+//primitive return types & co-variant return types in overriding
 
 #include<iostream>
+using namespace std;
+
+class Parent{
+    public:
+        virtual int getData(){
+            cout<<"Parent getData\n";
+            return 10;
+        }
+};
+
+class Child : public Parent{
+    public:
+        int getData(){
+            cout<<"Child getData\n";
+            return 10;
+        }
+};
 
 int main(){
-
+    Parent *p=new Child();
+    p->getData();
     return 0;
 }
