0968a28fcf9ccd88e1060a402e287b77e5414738
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-08-20 14:36:32 +0530

    prepInsta

---
+++ prepinsta/arrays/distinctElements.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5],size=5,count,visited[size];
+
+    cout<<"Eles: ";
+    
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<size;i++){
+        if(visited[i]!=1){
+            count=0;
+           for(int j=i+1;j<size;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                    visited[j]=1;
+                }
+            }
+        }
+
+        if(count<1){
+            cout<<"distinct ele: "<<arr[i];
+        }
+    }
+
+    return 0;
+}
