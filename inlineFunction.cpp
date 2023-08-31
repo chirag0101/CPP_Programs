995b65d448047cded559061b9f90a818b66c6816
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-09-01 00:30:51 +0530

    inlineFun

--- inlineFunction.cpp
+++ inlineFunction.cpp
@@ -1,11 +1,19 @@
-//inline function-> 
+//function like macros stops pushing up the stack frame as it is a heavy operation
 
 #include<iostream>
+#define x 500                                                                //object like macros
+#define fun(x,y)  x+y                                                    //function like macros
+
 using namespace std;
 
 int main(){
 
+    cout<<x<<"\n";                                                        //while pre-processing 500 gets replaced in place of x
 
+    cout<<fun(10,20);                                                     //while pre-processing function body i.e 10+20 gets replaced in place of fun(10,20)
 
     return 0;
 }
+
+/*to see what gets replaced at x
+g++ -E inlineFunction.cpp > inlineFunction.txt*/
---
+++ inlineFunction1.cpp
@@ -0,0 +1,17 @@
+//inline functions are same like function like macros but here compiler decides whether the function should be inline or not
+
+//function can't be inline if it contains: recursion, more lines of code, static variables, for loops,etc
+
+//no one can predict if a function can be inline or not 
+
+#include<iostream>
+using namespace std;
+
+inline int fun(int a,int b){
+    return a+b;
+}
+
+int main(){
+    cout<<fun(10,20)<<endl;
+    return 0;
+}
