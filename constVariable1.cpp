ee9b55fe6ba6af648b11e187a1fc6e8ac69eac05
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-08-31 23:48:46 +0530

    inlineFun

--- constVariable1.cpp
+++ constVariable1.cpp
@@ -12,9 +12,11 @@ int main(){
     int a=10,b=20;
 
     const int *ptr =&a;
-    a=30;                                   //variable data a becomes constant
+    a=30;                                   //data of value at ptr becomes constant means we can't do *ptr=30, value at ptr is const but actual a isn't constant
     cout<<*ptr<<endl;
-    ptr=&b;
+    *ptr=40;                               //not allowed as *ptr is consant
+    ptr=&b;                               //allowed as ptr isn't consant      
+
     cout<<*ptr<<endl;
 
     int *const ptr2=&a;             //making a pointer constant
--- inlineFunction.cpp
+++ inlineFunction.cpp
@@ -1,12 +1,11 @@
-//inline function
+//inline function-> 
 
 #include<iostream>
 using namespace std;
 
 int main(){
-    int y;
-    cin>>y;
-    const int x=y;
-    cout<<x;
+
+
+
     return 0;
 }
