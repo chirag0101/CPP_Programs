5a062d6b18e16af750a64769f92d370a2c7442a5
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-09-25 22:21:02 +0530

    exceptions

--- exceptionHandling/exception1.cpp
+++ exceptionHandling/exception1.cpp
@@ -1,1 +1,15 @@
 //exception stops/breaks the flow of the program
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+
+    cout<<"Enter two numbers: ";
+    cin>>x>>y;
+
+    cout<<x/y;                                  //here when we give 2nd number as 0 then it throws floating point exception
+
+    return 0;
+}
