13759caaf69b3b57eb5d122b01e7c5cab50c4a58
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-09-25 23:06:29 +0530

    exceptions

--- exceptionHandling/exception3.cpp
+++ exceptionHandling/exception3.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main(){
 
+    cout<<"Start Main"<<endl;
+
+    try{
+        //risky code we want to check if it throws an error
+
+        throw -1;
+
+    }catch(int x){
+        cout<<"Exception Handling"<<endl;
+    }
+
+    cout<<"End Main"<<endl;
+
     return 0;
 }
