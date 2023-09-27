482a09ef7786a1e80c92e57e777894e268855fc3
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-09-27 06:48:01 +0530

    exceptions

--- exceptionHandling/exception10.cpp
+++ exceptionHandling/exception10.cpp
@@ -5,8 +5,34 @@ class Emp{
     int id;
     string name;
     float sal;
+
+    public:
+        Emp(int id,string name,float sal){
+            this->id=id;
+            this->name=name;
+            this->sal=sal;
+        }
+
 };
 
 int main(){
+    Emp obj1(1,"chirag",10);
+    Emp obj2(2,"soham",5);
+    Emp obj3(3,"umesh",15);
+    Emp obj4(4,"alok",4);
+    Emp obj5(5,"rutul",3);
+
+    Emp arr[]={obj1,obj2,obj3,obj4,obj5};
+
+    Emp obj6;
+
+    try{
+        for(int i=0;i<5;i++){
+            cout<<arr[i.sal];
+        }
+    }catch(){
+
+    }
+
     return 0;
 }
---
+++ exceptionHandling/practice.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+class belowAvg{
+    public:
+        belowAvg(char const* str){
+            cout<<str<<"\n";
+        }
+};
+
+class aboveAvg{
+    public:
+        aboveAvg(char const* str){
+            cout<<str<<"\n";
+        }
+};
+
+class Emp{
+    public:
+        int id;
+        string name;
+        float sal;
+        
+        Emp(int id,string name,float sal){
+            this->id=id;
+            this->name=name;
+            this->sal=sal;
+        }
+
+        int operator[](int index){
+                return this->sal;
+        }
+
+        friend ostream& operator<<(ostream& cout,Emp obj){
+            cout<<obj.sal;
+            return cout;
+        }
+
+};
+
+int main(){
+    Emp obj1(1,"chirag",10);
+    Emp obj2(2,"soham",5);
+    Emp obj3(3,"umesh",15);
+    Emp obj4(4,"alok",4);
+    Emp obj5(5,"rutul",3);
+
+    Emp arr[]={obj1,obj2,obj3,obj4,obj5};
+
+    try{
+        for(int i=2;i<5;i++){
+            if(arr[i].sal<=5){
+                throw "belowAvg";
+            }else if(arr[i].sal>=15){
+                throw "aboveAvg";
+            }else{
+                cout<<"Avg sal";
+                break;
+            }
+        }
+    }catch(char const* str){
+        if(str=="belowAvg"){
+            belowAvg obj(str);
+        }else{
+            aboveAvg obj(str);
+        }
+    }
+
+    return 0;
+}
