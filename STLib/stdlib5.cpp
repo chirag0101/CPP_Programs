3227767847d1b0c493733443d9f9ad34744e1496
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-09-28 11:57:30 +0530

    exception

--- STLib/stdlib5.cpp
+++ STLib/stdlib5.cpp
@@ -4,7 +4,15 @@
 using namespace std;
 
 int main() {
-    vector<> vObj;
-    
+    vector<int> vObj={10,20,30,40,50};
+
+    vector<int>::iterator itr;                  //iterator for vector
+
+    //if we want to skip above declaration we can use 'auto' before 'itr' in the for loop at initialization
+
+    for(itr=vObj.begin();itr!=vObj.end();itr++){
+        cout<<*itr<<"\n";
+    }
+
     return 0;
 }
