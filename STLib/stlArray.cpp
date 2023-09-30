69ff3003c4b1e55d565669fc9f9fc595d363547c
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-09-30 17:14:23 +0530

    array

--- STLib/stlArray.cpp
+++ STLib/stlArray.cpp
@@ -1,9 +1,11 @@
-//also called as decay array
+//also called as decay array (fixed size array)
 
 #include<iostream>
 #include<array>
 using namespace std;
 
 int main(){
+    array<int,5> arr={10,20,30,40,50};           //requires the size of array as a parameter 
+    cout<<arr.at(1);
     return 0;
 }
