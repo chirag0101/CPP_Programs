c2bcbf4c53d446c16808c34050f82f471756ce20
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-10-05 19:29:39 +0530

    stl->queues

--- STLib/2_container_adapter_classes/stlQueue.cpp
+++ STLib/2_container_adapter_classes/stlQueue.cpp
@@ -4,7 +4,28 @@
 #include<queue>
 using namespace std;
 
-int main(){
+void showQ(queue<int>& obj,int size){
     
+    int i=0;
+    
+    while(i!=size){
+        cout<<obj.(front()++)<<" ";
+        i++;
+    }
+
+}
+
+int main(){
+    queue<int> q;
+    priority_queue<int> pq;
+
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+
+    showQ(q,q.size());
+
     return 0;
 }
