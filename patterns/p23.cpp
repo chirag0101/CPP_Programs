3accb60d47487f8d2dbeb889261592ca61c4b5f6
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-10-05 08:36:05 +0530

    pattern

--- patterns/p23.cpp
+++ patterns/p23.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main(){
+    int rows=4,l=7;
+    for(int i=1;i<=rows;i++){
+        for(int j=2;j<=i;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=l;k++){
+            cout<<k;
+        }
+        l=l-2;
+        cout<<"\n";
+    }
     return 0;
 }
