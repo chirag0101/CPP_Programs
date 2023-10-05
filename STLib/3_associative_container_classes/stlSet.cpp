87f539a7c2609d2f4d01a4a5336b75b3c3001269
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-10-05 12:41:43 +0530

    stl_forward_list

--- STLib/3_associative_container_classes/stlSet.cpp
+++ STLib/3_associative_container_classes/stlSet.cpp
@@ -9,7 +9,17 @@ int main(){
     set<int> :: iterator itr;
 
     for(itr=s.begin();itr!=s.end();itr++){
-        cout<<*itr<<" ";                                        //op: 5 10 20 removes duplicates & sorts them
+        cout<<*itr<<" ";                                            //op: 5 10 20 removes duplicates & sorts them
+    }
+
+    cout<<"\n";
+
+    set<int,greater<int>> s2={10,20,10,5,20};       //for sorting elements in descending order
+
+    set<int> :: iterator itr2;
+
+    for(itr2=s2.begin();itr2!=s2.end();itr2++){
+        cout<<*itr2<<" ";                                            //op: 5 10 20 removes duplicates & sorts them
     }
 
     return 0;
