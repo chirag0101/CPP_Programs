96a2548c066fa55a2d2c41d4f3db6732837abd27
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-10-05 23:54:14 +0530

    stl->ulsets

--- STLib/4_unordered_associative_container_classes/stlUnorderedSet.cpp
+++ STLib/4_unordered_associative_container_classes/stlUnorderedSet.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
+#include<unordered_set>
 using namespace std;
 
 int main(){
-    
+    unordered_set<int> unS={10,20,30,5,50};
+
+    unordered_set<int> :: iterator itr;
+
+    for(itr=unS.begin();itr!=unS.end();itr++){
+        cout<<*itr<<" ";
+    }
+
+    cout<<"\n";
+
+    unordered_multiset<string> unS2;
+
+    unordered_multiset<string> :: hasher obj=unS2.hash_function();
+
+    cout<<obj("A")<<"\n";
+
     return 0;
 }
