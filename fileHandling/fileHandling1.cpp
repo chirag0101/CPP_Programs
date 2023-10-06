053348663e48185250c4ff424f2aeba23b75beec
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-10-06 19:26:53 +0530

    fileHandling

--- fileHandling/fileHandling1.cpp
+++ fileHandling/fileHandling1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
     ofstream outfile("file.txt");           //outfile is a constructor that takes a parameter to open a file, if not present it creates a file
 
-    outfile<<"chirag\n";                       //this is the way to add something in the file, it appends at the end of the file
+    outfile<<"chirag\n";                    //this is the way to add something in the file, it appends at the end of the file
     outfile<<"rupali\n";
     outfile<<"sanjay\n";
     outfile<<"suryawanshi\n";          
