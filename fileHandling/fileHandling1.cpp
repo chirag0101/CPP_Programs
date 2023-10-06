a7d5b281b4263e0d839db5bfd7aa9c84780b3ab5
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-10-06 18:58:54 +0530

    fileHandling

---
+++ fileHandling/fileHandling1.cpp
@@ -0,0 +1,18 @@
+//ofstream-constructor
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+    ofstream outfile("file.txt");           //function to open a file, if not present it creates a file
+
+    outfile<<"chirag\n";                       //this is the way to add something in the file, it appends at the end of the file
+    outfile<<"rupali\n";
+    outfile<<"sanjay\n";
+    outfile<<"suryawanshi\n";
+
+    outfile.close();                              //close the opening file
+
+    return 0;
+}
