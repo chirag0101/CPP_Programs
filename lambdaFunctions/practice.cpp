2cc905e084abca4a18556131fc8f3806eb14033d
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-10-10 18:47:11 +0530

    lambda func

--- lambdaFunctions/practice.cpp
+++ lambdaFunctions/practice.cpp
@@ -3,7 +3,28 @@ using namespace std;
 
 int main(){
 
-    
+    int a=10,b=20;
+
+    auto add = [=] (int x,int y) -> int{
+        cout<<a<<" "<<b<<"\n";
+        return x+y;
+    };
+
+    /*the above code goes as:
+            
+            class Demo{
+                int a,b;
+                Demo(int a,int b){
+                    this->a=a;
+                    this->b=b;
+                }
+
+                int operator(int x,int y){
+                    return x+y;
+                }
+            };
+            
+    */
 
     return 0;
 }
