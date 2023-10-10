1125691d5fb7eb7ed46cf1802186c8686817203a
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-10-10 17:13:22 +0530

    lambda func

---
+++ lambdaFunctions/lambdaFunctions1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    [](){
+        cout<<"In Lambda Function";
+    }();    // ( ) means a function call to the above function itself as it doesn't have any name & can't be called later
+
+}
