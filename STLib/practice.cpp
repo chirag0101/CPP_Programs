1d13a9d6c2e484a52d041679c3b05ab5de60eaba
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-01-16 16:24:38 +0530

    practice

--- STLib/practice.cpp
+++ STLib/practice.cpp
@@ -1,17 +1,58 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
-
 using namespace std;
 
 int main(){
-    int arr[]={50,40,30,20,10};
-    string s="123";
-    sort(s.begin(),s.end());
-    cout<<s;
+    int arr1[]={10,20,40};
+    int arr2[]={20,50};
 
-    do{
-        cout<<s<<" ";
-    }while(next_permutation(s.begin(),s.end()));
-    return 0;
+    int size1=sizeof(arr1)/sizeof(arr1[0]);
+    int size2=sizeof(arr2)/sizeof(arr2[0]);
+    int totalSize=size1+size2;
+
+    int arr3[totalSize];
+
+    int i=0,j=0,k=0;
+
+    while(i<size1 && j<size2){
+        if(arr1[i]<arr2[j]){
+            arr3[k++]=arr1[i++];
+        }else if(arr1[i]>arr2[j]){
+            arr3[k++]=arr2[j++];
+        }else{
+            arr3[k++]=arr1[i++];
+        }
+    }
+
+    while(i<size1){
+        arr3[k++]=arr1[i++];
+    }
+
+    while(j<size2){
+        arr3[k++]=arr2[j++];
+    }
+
+    for(int l=0;l<size1+size2;l++){
+        cout<<arr3[l]<<" ";
+    }    
+
 }
+
+// void rev(vector<int> &arr1,int m,vector<int> &arr2,int n){
+    
+// }
+
+// int main(){
+//     vector<int> v1={1,2,3,4,5};
+//     int size1=v.size();
+
+//     vector<int> v2={1,2,3,4,5};
+//     int size2=v.size();
+
+//     rev(v1,size1,v2,size2);
+
+//     for(int i=0;i<size;i++){
+//         cout<<v[i]<<" ";
+//     }
+
+// }
