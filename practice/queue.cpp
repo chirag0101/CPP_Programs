77c6bd3d03fe94967ed81a547d1ed64b533ff78a
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-06-13 18:31:21 +0530

    queue

--- practice/queue.cpp
+++ practice/queue.cpp
@@ -10,15 +10,35 @@ void queue(int data){
     if(front==-1){
         front++;
     }
-    
     rear++;
+    arr[rear]=data;
 }
+
+void dequeue(){
+    if(front==-1){
+        cout<<"Queue Empty!"<<endl;
+    }else{
+        front++;
+    }
+}
+
+void display(){
+    if(front==-1){
+        cout<<"Queue Empty!";
+    }else{
+        for(int i=front;i<=rear;i++){
+            cout<<arr[i]<<" | ";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     while(1){
-        cout<<"1. queue(insert)";
-        cout<<"2. dequeue(delete)";
-        cout<<"3. display";
-        cout<<"4. end";
+        cout<<"1. queue(insert)\n";
+        cout<<"2. dequeue(delete)\n";
+        cout<<"3. display\n";
+        cout<<"4. end\n";
         int ch;
         cin>>ch;
 
@@ -28,7 +48,18 @@ int main(){
                 cout<<"Enter data to queue:";
                 cin>>data;
                 queue(data);
+                break;
             }
+            case 2:
+                dequeue();
+                break;
+            case 3:{
+                display();
+                break;
+            }
+            case 4:
+                exit(0);
+                break;
         }
     }
 }
