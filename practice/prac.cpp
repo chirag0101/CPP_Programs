bcc3adecea9cd17f657a01a99897987e37f6cf0a
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-06-17 22:37:26 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -1,15 +1,8 @@
 #include<iostream>
 using namespace std;
 
-void func(int* ref){
-   *ref=20;
-}
-
 int main(){
-    int a=10;
-    int *ref=&a;
-    // int b=func(ref);
-    func(ref);
-    cout<<*ref;
-    return 0;
+    int a;
+    cout<<a;
+    return -1;
 }
