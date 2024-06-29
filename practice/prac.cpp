9c839b4995f821f1aeedb074ae11ee52e84f435a
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-06-29 17:31:41 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -2,11 +2,26 @@
 using namespace std;
 
 class stud{
-    int name;
-    string namse;
+    // int name;
+    // string namse;
+    public:
+    int name=10;
+    int fun(int name){
+        name=name;
+        return name;
+    }
+
+    stud(){
+        
+    }
+
+    stud(stud &obj){
+        name=obj.name;
+    }
 };
 int main(){
     stud s1;
-    cout<<sizeof(s1);
+    stud s2(s1);
+    cout<<s2.name;
     return 0;
 }
