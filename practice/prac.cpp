30df9c9883a7c02490c865cf4acca9f1fc5a15ed
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-06-30 22:42:04 +0530

    deep vs shallow copy

---
+++ practice/prac.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+class Student{
+    public:
+        int rollNo;
+        std::string name;
+        int *ptr;
+
+        Student(){
+            ptr=new int();
+        }
+
+        Student(Student &obj){
+            this->rollNo=obj.rollNo;
+            this->name=obj.name;
+            this->ptr=new int();
+        }
+
+    
+
+};
+
+int main(){
+    Student s1;
+    s1.rollNo=10;
+    
+    Student s2(s1);
+    *(s2.ptr)=20;
+
+    std::cout<<s1.rollNo;
+    return 0;
+}
