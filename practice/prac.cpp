bc3296c2811c0003d691d4c16e060ca0ed76efa8
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-01 22:52:09 +0530

    static keyword

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -1,52 +1,15 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main ()
-{
-    /*
-        ********
-        ***    ***
-        **       **    
-        *          *
-        **       **
-        ***    ***
-        ********
-    */
-    int l=2;
-    for(int i=1;i<=4;i++){
-        for(int j=4;j>=i;j--){
-            cout<<"*";
-        }
-       
-        for(int k=2;k<=l;k++){
-            cout<<" ";
-        }
-        l=l+2;
-       
-        for(int p=4;p>=i;p--){
-            cout<<"*";
-        }
-        
-            cout<<"\n";
-       
-    }
-   
-    int a=4;
-    for(int i=2;i<=4;i++){
-        for(int j=1;j<=i;j++){
-            cout<<"*";
-        }
-       
-        for(int k=a;k>=0;k--){
-            cout<<" ";
-        }
-        a=a-2;
-       
-        for(int x=1;x<=i;x++){
-            cout<<"*";
-        }
-        cout<<"\n";
-    }
+void fun(int a){
+    static int b=1;
+    cout<<"A: "<<a<<" B: "<<b<<endl;
+    b++;
+}
 
- return 0;
+int main(){
+    int a=1;
+    fun(a);
+    fun(a);
+    return 0;
 }
