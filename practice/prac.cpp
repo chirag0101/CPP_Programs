75047ec7abdee1146fc12c671162d085ff1ed01d
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-01 19:23:15 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -1,28 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class A{
-    public:
-     void fun(){
-        cout<<"A::fun()"<< endl;
-     }
-};
-
- class B : virtual public A{
+int main ()
+{
+    /*
+        ********
+        ***    ***
+        **       **    
+        *          *
+        **       **
+        ***    ***
+        ********
+    */
+    int l=2;
+    for(int i=1;i<=4;i++){
+        for(int j=4;j>=i;j--){
+            cout<<"*";
+        }
+       
+        for(int k=2;k<=l;k++){
+            cout<<" ";
+        }
+        l=l+2;
+       
+        for(int p=4;p>=i;p--){
+            cout<<"*";
+        }
+        
+            cout<<"\n";
+       
+    }
    
-};
+    int a=4;
+    for(int i=2;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+       
+        for(int k=a;k>=0;k--){
+            cout<<" ";
+        }
+        a=a-2;
+       
+        for(int x=1;x<=i;x++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
 
- class C : virtual public A{
-      
-};
-
-class D : virtual public B, virtual public C{
-    public:
-};
-
-
-int main(){
-    D* d=new D{};
-    d->fun();
-    return 0;
+ return 0;
 }
