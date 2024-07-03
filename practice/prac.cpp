1e3a6b5539651b2512e169aa8e2e18cc36267c4c
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-03 21:26:01 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -3,30 +3,22 @@ using namespace std;
 
 int main(){
     int n=3;
-    for(int i=0;i<n;i++){
-        for(int k=n;k>i+1;k--){
-            cout<<" ";
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            std::cout<<j<<"";
         }
-        for(int j=0;j<=i;j++){
-            cout<<"*";
+        for(int k=n;k>i;k--){
+            std::cout<<" ";
         }
-        for(int l=0;l<i;l++){
-            cout<<"*";
+        for(int k=n;k>i;k--){
+            std::cout<<" ";
         }
-        cout<<endl;
-    }
-    for(int i=0;i<n;i++){
-        for(int k=0;k<i;k++){
-            cout<<" ";
-        }
-        for(int j=n;j>i;j--){
-            cout<<"*";
-        }
-        for(int l=n;l>i+1;l--){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
+        int num=i;
+        for(int l=1;l<=i;l++){
 
+            std::cout<<num--<<"";
+        }
+        std::cout<<std::endl;
+    }
     return 0;
 }
