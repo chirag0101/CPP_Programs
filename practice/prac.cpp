a3c451ce6e84dcae3b2353b008a78b58b685aaaa
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-06 00:19:20 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -3,39 +3,39 @@ using namespace std;
 
 int main(){
     int size;
-    cout<<"Enter the size of the array: ";
+    cout<<"Enter Size:";
     cin>>size;
-    int arr[size];
 
-    cout<<"Enter Array Elements:";
-    for(int i=0;i<size;i++){
+    int arr[size];
+    cout<<"Enter Elements:";
+    for(int i=0;i<size;i++){  
         cin>>arr[i];
     }
 
-    cout<<"Before Sorting: ";
+    cout<<"Before Swap:";
     for(int i=0;i<size;i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
 
-    for(int i=0;i<size-1;i++){          
-    int small=i;
-
-        for(int j=i+1;j<size;j++){
-            if(arr[j]<arr[small]){
-                small=j;
-            }
+    for(int i=1;i<size;i++){
+        int j=i-1;
+        int store=arr[i];
+        while(j>=0 && store<arr[j]){
+            arr[j+1]=arr[j];
+            j--;
         }
-        swap(arr[i],arr[small]);
+        arr[j+1]=store;
         for(int i=0;i<size;i++){
             cout<<arr[i]<<" ";
         }
         cout<<endl;
     }
 
-    cout<<"After Sorting:";
+    cout<<"After Swap:";
     for(int i=0;i<size;i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
+    return 0;
 }
