6bca407094e86413e36b642ebb660da564ad952f
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-05 14:52:36 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -1,6 +1,13 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 int main()
 {
-    char str[5] = "IndiaBIX";
+    int *p;
+    printf("%u ", p);
+
+    p = (int *)malloc(20); /* Assume p has address of 1314 */
+    free(p);
+    printf("%u", p);
     return 0;
 }
