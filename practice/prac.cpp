9c65c382575e9723fbf95051a7749d751d436904
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-06 22:47:50 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -1,41 +1,10 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    int size;
-    cout<<"Enter Size:";
-    cin>>size;
-
-    int arr[size];
-    cout<<"Enter Elements:";
-    for(int i=0;i<size;i++){  
-        cin>>arr[i];
-    }
-
-    cout<<"Before Swap:";
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-
-    for(int i=1;i<size;i++){
-        int j=i-1;
-        int store=arr[i];
-        while(j>=0 && store<arr[j]){
-            arr[j+1]=arr[j];
-            j--;
-        }
-        arr[j+1]=store;
-        for(int i=0;i<size;i++){
-            cout<<arr[i]<<" ";
-        }
-        cout<<endl;
-    }
-
-    cout<<"After Swap:";
-    for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+#include<stdio.h>
+int main()
+{
+    char ch;
+    if(ch = printf(""))
+        printf("It matters\n");
+    else
+        printf("It doesn't matters\n");
     return 0;
 }
