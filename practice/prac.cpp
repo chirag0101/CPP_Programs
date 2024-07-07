8e280a51778d17908c1f52847c0f240e7ef5b577
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-07 15:44:00 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -1,18 +1,9 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 
-int main()
-{
-    void fun(char*);
-    char a[100];
-    a[0] = 'A'; a[1] = 'B';
-    a[2] = 'C'; a[3] = 'D';
-    fun(&a[0]);
+int main(){
+    int a[]={1,2,3};
+    int *p=a;
+    cout<<p[2];
     return 0;
-}
-void fun(char *a)
-{
-    a++;
-    printf("%c", *a);
-    a++;
-    printf("%c", *a);
 }
