081f755e5cb8bfd1f8a74a953a621f2e8ca5559f
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-07 19:40:28 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -1,9 +1,51 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
-    int a[]={1,2,3};
-    int *p=a;
-    cout<<p[2];
+    vector<int> nums={1,2,3,4,5,6,7};
+    int k=3;
+
+    int i=0;
+    int j=k-1;
+
+    while(i<j){
+        swap(nums[i],nums[j]);
+        i++;
+        j--;
+    }
+    for(int k=0;k<nums.size();k++){
+        cout<<nums[k]<<" ";
+    }
+    cout<<endl;
+
+    i=k;
+    j=nums.size()-1;
+
+    while(i<j){
+        swap(nums[i],nums[j]);
+        i++;
+        j--;
+    }
+
+    for(int k=0;k<nums.size();k++){
+        cout<<nums[k]<<" ";
+    }
+    cout<<endl;
+
+    i=0;
+    j=nums.size()-1;
+
+    while(i<j){
+        swap(nums[i],nums[j]);
+        i++;
+        j--;
+    }
+    for(int k=0;k<nums.size();k++){
+        cout<<nums[k]<<" ";
+    }
+    cout<<endl;
+
+
     return 0;
 }
