bcbfe622aec8ad26456daeb1526b98cf19998cc9
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-10 00:15:19 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -1,20 +1,10 @@
 #include<stdio.h>
-
+#include<iostream>
+using namespace std;
 int main()
 {
-    void fun(int, int[]);
-    int arr[] = {1, 2, 3, 4};
-    int i;
-    fun(4, arr);
-    for(i=0; i<4; i++)
-        printf("%d,", arr[i]);
+    int arr[2];
+    *(arr+3)=10;
+    cout<<*(arr+3);
     return 0;
-}
-void fun(int n, int arr[])
-{
-    int *p=0;
-    int i=0;
-    while(++i < n)
-        p = &arr[i];
-    *p=0;
 }
