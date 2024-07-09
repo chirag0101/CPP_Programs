53c0e4de686b0f984ae054fa5fcc7b73143c17d8
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-10 01:48:54 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -1,10 +1,28 @@
 #include<stdio.h>
 #include<iostream>
 using namespace std;
+
+// int fun(int *ptr,int size){
+//     int sum=0;
+//     for(int i=0;i<size;i++){
+//         cout<<(*(ptr+1))<<" ";
+//     }
+//     return sum;
+// }
 int main()
 {
-    int arr[2];
-    *(arr+3)=10;
-    cout<<*(arr+3);
+    // int arr[]={1,2,3};
+    // int sum=fun(arr,3);
+    // cout<<sum<<endl;
+    // int arr1[3]={1,2,3,4};
+    int arr2[2][2]={{1,2},{5,6}};
+    int size=sizeof(arr2)/sizeof(arr2[0]);
+    // cout<<arr1[3]<<endl;
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            cout<<arr2[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
