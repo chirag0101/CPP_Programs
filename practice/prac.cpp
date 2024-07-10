691a34e975ec7dbf7c02e93c989b5212bf21748a
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-10 12:16:49 +0530

    pointers

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -1,18 +1,8 @@
 #include<stdio.h>
-#include<iostream>
-using namespace std;
 
-int fun(int *ptr,int size){
-    int sum=0;
-    for(int i=0;i<size;i++){
-        sum=sum+*(ptr+i);
-    }
-    return sum;
-}
 int main()
 {
-    int arr[]={5,2,7};
-    int sum=fun(arr,3);
-    cout<<sum<<endl;
+    float a=3;
+    printf("%3f\n", a);
     return 0;
 }
