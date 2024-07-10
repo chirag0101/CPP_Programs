c75fbb6fca3e1a60fa10435132acc6fda2744da6
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-10 18:36:10 +0530

    reverseByKPos

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -1,8 +1,41 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 
-int main()
-{
-    float a=3;
-    printf("%3f\n", a);
+int main(){
+    int arr[]={1,2,3,4,5};
+    int k=3;
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int i=0;
+    int j=size-k-1;
+    
+    while(i<j){
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
+    }
+
+    i=size-k;
+    j=size-1;
+
+    while(i<j){
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
+    }
+
+    i=0;
+    j=size-1;
+
+    while(i<j){
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
+    }
+
+    for(int a=0;a<size;a++){
+        cout<<arr[a]<<" ";
+    }
+    cout<<endl;
+
     return 0;
 }
