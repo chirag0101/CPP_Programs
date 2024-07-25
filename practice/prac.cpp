fc1aef6502fee2e363f2cd4ee93a7321b9ef1af9
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2024-07-25 22:39:44 +0530

    prac

--- practice/prac.cpp
+++ practice/prac.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
-    printf("India""BIX\n");
+    int num=75;
+    int discount=5;
+    int price=50;
+    int cost=30;
+    int profit=((price-(profit*5))-cost);
+    cout<<profit<<" ";
     return 0;
 }
